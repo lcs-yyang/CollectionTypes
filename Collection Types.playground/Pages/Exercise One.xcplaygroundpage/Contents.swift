6c17bf71219784d931d6f05a442fe223b59782ed
Yang, Yuxin <yyang@lcs-mba3025.lcs.on.ca>
2018-02-15 11:23:43 -0500

    Finished both exercise 1 and 2

--- Collection Types.playground/Pages/Exercise One.xcplaygroundpage/Contents.swift
+++ Collection Types.playground/Pages/Exercise One.xcplaygroundpage/Contents.swift
@@ -8,22 +8,32 @@
  */
 
 import Foundation
-
+var grades :[Double] = []
 
 /*:
  2. Now populate the array with eight random grades between 60 and 100.
  */
-
+for _ in 1...8 {
+    grades.append(Double( arc4random_uniform(40) + 60 ))
+}
 
 /*:
  3. Print a list of each array index and the associated value.
  */
+print("There are \(grades) values in the array right now.")
 
 
 /*:
  4. Determine and print the highest grade.
  */
+var highest = 100.0
 
+for grades in grades {
+    if grades < highest {
+        highest = grades
+    }
+}
+print("The highest grades is \(highest).")
 
 //: [Next](@next)
 
--- Collection Types.playground/Pages/Exercise Two.xcplaygroundpage/Contents.swift
+++ Collection Types.playground/Pages/Exercise Two.xcplaygroundpage/Contents.swift
@@ -10,21 +10,34 @@
  */
 
 import Foundation
-
-
-/*:
- 2. Now populate the array with twenty random ratings between 1 and 100.
- */
+var restaurantRating : [Double] = []
+for _ in 1...20 {
+    restaurantRating.append(Double( arc4random_uniform(99) + 1 ))
+}
 
 
 /*:
  3. Determine the average rating. Remember, calculate an average by finding the sum of all the values, then dividing by the number of values.
  */
-
-
+var total = 0.0
+for rating in restaurantRating {
+    print(rating)
+    total += rating
+}
+total /= 20
+print(total)
 /*:
  4. The restaurant is most concerned with how many diners considered their meal to be less than satisfactory. Calculate the percentage of diners who gave the restaurant a rating less than 80.
  */
+var highest = 80.0
+var ratingCount = 0.0
+for rating in restaurantRating {
+    if rating < highest {
+        ratingCount += 1
+    }
+}
+(ratingCount / 20) * 100
+print(ratingCount)
 
 /*:
  5. The manager wants more detail. Prepare a report that indicates how many diners found the restuarant "Very Bad" (0 to 25), "Bad" (25 to 49), "Fair" (50 to 79), "Satisfactory" (80 to 89), and "Stellar" (90 to 100).
